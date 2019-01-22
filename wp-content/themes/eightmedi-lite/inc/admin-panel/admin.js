9fe3feb9103879ef9859522d5bc91dd7d1abde39
soki-chrisna <eric.soki@gmail.com>
2019-01-23 01:37:54 +0800

    initial commit

---
+++ wp-content/themes/eightmedi-lite/inc/admin-panel/admin.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+
+	$('#accordion-panel-eightmedi_lite_default_setups').prepend(
+		'<div class="user_sticky_note">'+
+		'<h3 class="sticky_title">Need help?</h3>'+
+		'<span class="sticky_info_row"><label class="row-element">View demo: </label> <a href="http://8degreethemes.com/demo/8medi-lite/" target="_blank">here</a>'+
+		'<span class="sticky_info_row"><label class="row-element">View documentation: </label><a href="http://8degreethemes.com/documentation/8medi-lite/" target="_blank">here</a></span>'+
+		'<span class="sticky_info_row"><label class="row-element">Support forum: </label><a href="https://8degreethemes.com/support/forum/free-themes/eightmedi-lite/" target="_blnak">here</a></span>'+
+		'<span class="sticky_info_row"><label class="row-element">Email us: </label><a href="mailto:support@8degreethemes.com">support@8degreethemes.com<a/></span>'+
+		'<span class="sticky_info_row"><label class="more-detail row-element">More Details: </label><a href="https://8degreethemes.com/wordpress-themes/" target="_blank">here</a></span>'+
+		'</div>'
+		);
+
+});
